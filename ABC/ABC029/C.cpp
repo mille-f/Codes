a682b05c3da76dfa0a88892544fbaa00ddbd421a
mille-f <f11235tetra@gmail.com>
2015-09-20 01:10:38 +0900

    solve D

---
+++ ABC/ABC029/C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+void func(int num, string str) {
+  if ( num == 0 ) { cout << str << endl; }
+  else {
+    for ( char ch = 'a'; ch <= 'c'; ch++ ) {
+      func(num-1, str+ch);
+    }
+  }
+}
+
+int main(void) {
+  int n;
+  cin >> n;
+
+  func(n,"");
+
+  return 0;
+}
