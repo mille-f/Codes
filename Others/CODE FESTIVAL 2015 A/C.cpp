30e3075299a3b5672bf3a98e22586c29e807f503
mille-f <f11235tetra@gmail.com>
2015-09-27 09:33:49 +0900

    solve C

---
+++ Others/CODE FESTIVAL 2015 A/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+  int n, t;
+  cin >> n >> t;
+  VI diff(n);
+  int ans = 0, sum = 0;
+  int a, b;
+
+  REP(i,n) {
+    cin >> a >> b;
+    sum += a;
+    diff[i] = a - b;
+  }
+
+  SORT(diff);
+  reverse(diff.begin(), diff.end());
+
+  REP(i,n) {
+    if ( sum <= t ) break;
+    else {
+      sum -= diff[i];
+      ans++;
+    }
+  }
+
+  if ( sum > t ) { cout << -1 << endl; }
+  else { cout << ans << endl; }
+
+  return 0;
+}
