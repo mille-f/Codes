1966e822d6d9a7ebe7f2048aba458b5e1858062f
mille-f <f11235tetra@gmail.com>
2015-10-11 00:03:28 +0900

    solve A

---
+++ ARC/ARC045/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+  string str;
+  bool fst = true;
+
+  while ( cin >> str ) {
+    if ( !fst ) cout << " ";
+    if ( str == "Left" ) cout << "<";
+    else if ( str == "Right" ) cout << ">";
+    else cout << "A";
+    fst = false;
+  } puts("");
+
+  return 0;
+}
