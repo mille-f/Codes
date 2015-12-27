c5de36a63016728220e090f2fe400eac53544681
mille-f <f11235tetra@gmail.com>
2015-12-28 04:19:08 +0900

    [add] atcoder.sh

---
+++ atcoder.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+read -p "Please input directory name : " name
+
+case $name in
+    ARC* )
+        mkdir "./ARC/$name"
+        cd "./ARC/$name";;
+    ABC* )
+        mkdir "./ABC/$name"
+        cd "./ABC/$name";;
+    *)
+        mkdir "./Others/$name"
+        cd "./Others/$name";;
+esac
+
+echo "Directory created!"
+
+exec /bin/zsh
