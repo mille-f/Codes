986c1d1eaa2735a900bb8e4c696c5ab1848c6d4c
mille-f <f11235tetra@gmail.com>
2015-12-27 16:17:53 +0900

    solved B

---
+++ ABC/ABC026/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+  int n;
+  cin >> n;
+  double ans = 0.0;
+  VI r(n);
+
+  REP(i,n) cin >> r[i];
+  SORT(r);
+
+  REP(i,n) {
+    if (i%2 == 0) {
+      ans += pow(r[i],2);
+    } else {
+      ans -= pow(r[i],2);
+    }
+  }
+
+  printf("%.10f\n", ABS(ans*PI));
+
+  return 0;
+}
