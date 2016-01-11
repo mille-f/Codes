56a41f570ed1deb83d9b20eb440ca0e21d09a94b
mille-f <f11235tetra@gmail.com>
2016-01-11 11:48:42 +0900

    solved A

---
+++ ABC/ABC032/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int gcd(int a, int b) {
+  int r;
+
+  while ( (r = a % b) != 0 ) {
+    a = b;
+    b = r;
+  }
+
+  return b;
+}
+
+int main(void) {
+  int a, b, n;
+  int r;
+  cin >> a >> b >> n;
+
+  r = gcd(a,b);
+
+  int i = 1;
+  while ( 1 ) {
+    if ( (a*b)*i/r >= n ) {
+      cout << (a*b)*i/r << endl; break;
+    }
+    i++;
+  }
+
+  return 0;
+}
