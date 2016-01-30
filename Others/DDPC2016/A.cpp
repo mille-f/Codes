1cd3ab2e784edd68a49d7bd9c5677bbdd834bbd1
mille-f <f11235tetra@gmail.com>
2016-01-31 00:56:42 +0900

    solved A

---
+++ Others/DDPC2016/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+  int w;
+  string str = "DiscoPresentsDiscoveryChannelProgrammingContest2016";
+  cin >> w;
+
+  for ( int i = 0; i < str.size(); i++ ) {
+    printf("%c", str[i]);
+    if ( i%w == w-1 && i != str.size()-1 ) puts("");
+  } puts("");
+
+  return 0;
+}
