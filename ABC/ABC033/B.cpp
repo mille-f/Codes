0bdd9acbeffca29ddea3461641394206d4b38dc6
mille-f <f11235tetra@gmail.com>
2016-02-06 21:30:51 +0900

    solved B

---
+++ ABC/ABC033/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x) (x).begin(),(x).end()
+#define ABS(x) ((x < 0) ? -(x) : (x))
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define MP make_pair
+#define PB push_back
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
+#define SIZE(x) int((x).size())
+
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const double INF = 1e100;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+const int mod = 1000000007;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+  int n;
+  int sum = 0;
+  cin >> n;
+  vector<string> s(n);
+  VI p(n);
+
+  REP(i,n) { cin >> s[i]; cin >> p[i]; sum += p[i]; }
+  REP(i,n) {
+    if ( p[i] > sum/2 ) { cout << s[i] << endl; return 0; }
+  }
+  cout << "atcoder" << endl;
+
+  return 0;
+}
