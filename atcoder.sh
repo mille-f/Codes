0a1b652759313f810fcd412d93e4733ae78f537d
mille-f <f11235tetra@gmail.com>
2018-01-14 00:23:56 +0900

    update

--- atcoder.sh
+++ atcoder.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
 read -p "Please input directory name : " name
+read -p "Please input using language [rb, c, cpp] : " lang
 
 case $name in
     ARC* )
         mkdir -p "./ARC/$name"
-        cd "./ARC/$name";;
+        cd "./ARC/$name"
+        touch A.$lang B.$lang C.$lang D.$lang;;
     ABC* )
         mkdir -p "./ABC/$name"
-        cd "./ABC/$name";;
+        cd "./ABC/$name"
+        touch A.$lang B.$lang C.$lang D.$lang;;
     ATC* )
         mkdir -p "./ATC/$name"
-        cd "./ATC/$name";;
+        cd "./ATC/$name"
+        touch A.$lang B.$lang C.$lang D.$lang;;
     AGC* )
         mkdir -p "./AGC/$name"
-        cd "./AGC/$name";;
+        cd "./AGC/$name"
+        touch A.$lang B.$lang C.$lang D.$lang;;
     *)
         mkdir -p "./Others/$name"
-        cd "./Others/$name";;
+        cd "./Others/$name"
+        touch A.$lang B.$lang C.$lang D.$lang;;
 esac
 
-echo "Directory created!"
+echo "Completed!"
 
 exec /bin/zsh
