f8b75c8e147a05cbd4f353b5013a7b19d32b2eef
mille-f <f11235tetra@gmail.com>
2018-01-25 02:16:16 +0900

    solved B

--- ABC/ABC006/B.rb
+++ ABC/ABC006/B.rb
@@ -1,10 +1,8 @@
 n = gets.to_i
 a = [0,0,1]
 
-i = 3
-while i < n do
-  a << a[i-1]+a[i-2]+a[i-3]
-  i += 1
+for i in 3..n-1 do
+  a[i] = (a[i-1]+a[i-2]+a[i-3])%10007
 end
 
-puts a[n-1]%10007
+puts a[n-1]
