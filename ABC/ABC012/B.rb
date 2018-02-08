17aedc35a1e802304ca89bd19ac2448f42ec2036
mille-f <f11235tetra@gmail.com>
2018-02-09 02:45:02 +0900

    simplify

--- ABC/ABC012/B.rb
+++ ABC/ABC012/B.rb
@@ -1,24 +1,4 @@
 n = gets.chomp.to_i
-hh, mm, ss = 0, 0, 0
-
-if n >= 3600
-  hh = n/3600
-  n -= 3600*hh
-  if n >= 60
-    mm = n/60
-    n -= 60*mm
-    ss = n
-  end
-end
-
-if n < 3600 && n >= 60
-  mm = n/60
-  n -= 60*mm
-  ss = n
-end
-
-if n < 60
-  ss = n
-end
+hh, mm, ss = n/3600, (n/60)%60, n%60
 
 puts sprintf("%02d:%02d:%02d", hh, mm, ss)
