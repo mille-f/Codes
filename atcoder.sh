2d5c36da16aab254779625395e71ff79f7739ce1
mille-f <f11235tetra@gmail.com>
2018-02-26 10:32:21 +0900

    [modify] don't create C, D

--- atcoder.sh
+++ atcoder.sh
@@ -7,23 +7,23 @@ case $name in
     ARC* )
         mkdir -p "./ARC/$name"
         cd "./ARC/$name"
-        touch A.$lang B.$lang C.$lang D.$lang;;
+        touch A.$lang B.$lang;;
     ABC* )
         mkdir -p "./ABC/$name"
         cd "./ABC/$name"
-        touch A.$lang B.$lang C.$lang D.$lang;;
+        touch A.$lang B.$lang;;
     ATC* )
         mkdir -p "./ATC/$name"
         cd "./ATC/$name"
-        touch A.$lang B.$lang C.$lang D.$lang;;
+        touch A.$lang B.$lang;;
     AGC* )
         mkdir -p "./AGC/$name"
         cd "./AGC/$name"
-        touch A.$lang B.$lang C.$lang D.$lang;;
+        touch A.$lang B.$lang;;
     *)
         mkdir -p "./Others/$name"
         cd "./Others/$name"
-        touch A.$lang B.$lang C.$lang D.$lang;;
+        touch A.$lang B.$lang;;
 esac
 
 echo "Completed!"
