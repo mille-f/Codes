c53946d3bd6b11a6f2fc3b7b13b3f10eb561d8ec
mille-f <f11235tetra@gmail.com>
2020-06-15 01:09:42 +0900

    C問題ファイルまで作成するように変更

--- atcoder.sh
+++ atcoder.sh
@@ -7,23 +7,23 @@ case $name in
     ARC* )
         mkdir -p "./ARC/$name"
         cd "./ARC/$name"
-        touch A.$lang B.$lang;;
+        touch A.$lang B.$lang C.$lang;;
     ABC* )
         mkdir -p "./ABC/$name"
         cd "./ABC/$name"
-        touch A.$lang B.$lang;;
+        touch A.$lang B.$lang C.$lang;;
     ATC* )
         mkdir -p "./ATC/$name"
         cd "./ATC/$name"
-        touch A.$lang B.$lang;;
+        touch A.$lang B.$lang C.$lang;;
     AGC* )
         mkdir -p "./AGC/$name"
         cd "./AGC/$name"
-        touch A.$lang B.$lang;;
+        touch A.$lang B.$lang C.$lang;;
     *)
         mkdir -p "./Others/$name"
         cd "./Others/$name"
-        touch A.$lang B.$lang;;
+        touch A.$lang B.$lang C.$lang;;
 esac
 
 echo "Completed!"
