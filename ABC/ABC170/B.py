a292cee1c317b72b3c1f00a02d3dd625e46850bb
mille-f <f11235tetra@gmail.com>
2020-06-16 03:03:48 +0900

    別解

--- ABC/ABC170/B.py
+++ ABC/ABC170/B.py
@@ -1,12 +1,21 @@
 x, y = map(int, input().split())
 
-turu = (4*x-y)//2
+#turu = (4*x-y)//2
 
-if (turu < 0) or (2*x > y):
-    print("No")
-    exit()
+# if (turu < 0) or (2*x > y):
+#    print("No")
+#    exit()
 
-if ((y-turu*2) % 4 == 0):
-    print("Yes")
-else:
-    print("No")
+# if ((y-turu*2) % 4 == 0):
+#    print("Yes")
+# else:
+#    print("No")
+
+ans = "No"
+for c in range(x+1):
+    t = x-c
+    if 2*c + 4*t == y:
+        ans = "Yes"
+        break
+
+print(ans)
